470ebc853de351a64548f314355653fc2ee271f0
Slávek Rydval <slavek@rydval.cz>
2018-07-18 21:45:24 +0200

    First commit

---
+++ LicenseFERDA/EAIntegration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LicenseFERDA
+{
+    public class EAIntegration
+    {
+        public string EA_Connect(EA.Repository Repository) => "";
+
+        public void EA_Disconnect()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+
+
+        private const string ValidLicense = "{FERDA}";
+        private const string SharedAddinName = "Ferda License";
+        private const string LicenseDescription = "Ferda License Test";
+
+        public bool IsEnteredValidLicense { get; private set; }
+
+        public bool EA_AddinLicenseValidate(EA.Repository Repository, string AddinKey)
+        {
+            IsEnteredValidLicense = AddinKey == ValidLicense;
+            return IsEnteredValidLicense;
+        }
+
+        public string EA_AddinLicenseGetDescription(EA.Repository Repository, string AddinKey) => LicenseDescription;
+        public string EA_GetSharedAddinName(EA.Repository Repository) => SharedAddinName;
+
+    }
+}
---
+++ LicenseMRAVENEC/EAIntegration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LicenseMRAVENEC
+{
+    public class EAIntegration
+    {
+        public string EA_Connect(EA.Repository Repository) => "";
+
+        public void EA_Disconnect()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+
+
+        private const string ValidLicense = "{MRAVENEC}";
+        private const string SharedAddinName = "Mravenc License";
+        private const string LicenseDescription = "Mravenc License Test";
+
+        public bool IsEnteredValidLicense { get; private set; }
+
+        public bool EA_AddinLicenseValidate(EA.Repository Repository, string AddinKey)
+        {
+            IsEnteredValidLicense = AddinKey == ValidLicense;
+            return IsEnteredValidLicense;
+        }
+
+        public string EA_AddinLicenseGetDescription(EA.Repository Repository, string AddinKey) => LicenseDescription;
+        public string EA_GetSharedAddinName(EA.Repository Repository) => SharedAddinName;
+    }
+}
